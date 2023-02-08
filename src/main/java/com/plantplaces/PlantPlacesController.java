f31081b6214da2ed3077dbd03a46fde0efa2feca
John Praiser <praiserjohn@gmail.com>
2023-02-08 15:59:44 -0500

    Updated Project
    Updated Project all should be working

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,13 +1,32 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
 
 @Controller
 public class PlantPlacesController {
+	@Autowired
+	private ISpecimenService specimenServiceStub;
 	
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read(Model model) {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		model.addAttribute("specimenDTO",specimenDTO);
+		return "start";
+	}
+	@PostMapping("/start")
+	public String create(){
+		return "start";
+	}
 	@RequestMapping("/start")
-	public String start() {
+	public String index() {
 		return "start";
 	}
 
